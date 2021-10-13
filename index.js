b81d4e07b3e0031d5aba22f48661a8cffe95b401
Tanveer Shah <tanvirhussein@gmail.com>
2021-10-13 15:42:18 -0400

    FEAT: API routes for users and posts created.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const PORT = 3000
 const express = require('express')
 const server = express()
 
+const {client} = require('./db')
+client.connect()
+
 const morgan = require('morgan')
 server.use(morgan('dev'))
 
